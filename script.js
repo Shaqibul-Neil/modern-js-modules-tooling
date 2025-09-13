6b10044e9164b08050dd1e99dd3b5d1dd7a02334
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-14 02:31:11 +0600

    importing all the exports

--- script.js
+++ script.js
@@ -2,17 +2,22 @@
 // import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js'; //imported value is executed first. modules doesn't need use strict
 
 //we can change the name also
-import {
-  addToCart,
-  totalPrice as price,
-  totalQuantity as quantity,
-} from './shoppingCart.js';
+// import {
+//   addToCart,
+//   totalPrice as price,
+//   qT as quantity, //totalQuantity->qT->quantity
+// } from './shoppingCart.js'from './shoppingCart.js';
 
-console.log('Importing Module');
-//console.log(shippingCost); /variables are scoped only in the module that it was declared. kind of encapsulated
-
-addToCart('breed', 5);
+//addToCart('breed', 5);
 //console.log(totalPrice, totalQuantity); //now we get the access of these values
 
 //after changing the name
-console.log(price, quantity);
+//console.log(price, quantity);
+
+//importing all the exports at the same time
+
+import * as ShoppingCart from './shoppingCart.js'; // * means everything-->importing everything into an object. capital case like class
+
+console.log('Importing Module');
+//console.log(shippingCost); /variables are scoped only in the module that it was declared. kind of encapsulated
+console.log(ShoppingCart);
--- shoppingCart.js
+++ shoppingCart.js
@@ -3,7 +3,7 @@ console.log('Exporting Module');
 
 const shippingCost = 10; //variables are scoped only in this module
 const cart = [];
-console.log(shippingCost);
+//console.log(shippingCost);
 
 //creating a named export--> func defined here exported and imported in script.js and then called there
 export const addToCart = (product, quantity) => {
@@ -25,3 +25,6 @@ if (true) { //---> it wont work here
 const totalPrice = 237;
 const totalQuantity = 23;
 export { totalPrice, totalQuantity }; //curly brace must
+
+//we can change the name here also
+//export { totalPrice, totalQuantity as qT };
