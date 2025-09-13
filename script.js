5279e2eaaefea4d098e8ac30d93bb21b90383f8b
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-14 02:03:38 +0600

    variables are scoped

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 //Importing Module
-import './shoppingCart.js';
+import './shoppingCart.js'; //imported value is executed first. modules doesn't need use strict
 console.log('Importing Module');
+//console.log(shippingCost); /variables are scoped only in the module that it was declared. kind of encapsulated
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,2 +1,6 @@
 //Exporting Module
 console.log('Exporting Module');
+
+const shippingCost = 10; //variables are scoped only in this module
+const cart = [];
+console.log(shippingCost);
