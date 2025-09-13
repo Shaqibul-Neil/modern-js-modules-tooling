ddf18a3cb6162915df3bb14ba0cc25fee1469fc7
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-14 02:11:25 +0600

    named export

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //Importing Module
-import './shoppingCart.js'; //imported value is executed first. modules doesn't need use strict
+import { addToCart } from './shoppingCart.js'; //imported value is executed first. modules doesn't need use strict
 console.log('Importing Module');
 //console.log(shippingCost); /variables are scoped only in the module that it was declared. kind of encapsulated
+
+addToCart('breed', 5);
--- shoppingCart.js
+++ shoppingCart.js
@@ -4,3 +4,11 @@ console.log('Exporting Module');
 const shippingCost = 10; //variables are scoped only in this module
 const cart = [];
 console.log(shippingCost);
+
+//creating a named export--> func defined here exported and imported in script.js and then called there
+export const addToCart = (product, quantity) => {
+  cart.push({ product, quantity });
+  console.log(`${product} ${quantity} added to the cart`);
+};
+
+//export always needs to be top level code, no block or function
